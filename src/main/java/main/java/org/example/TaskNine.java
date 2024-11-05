bece5533892cb411ab786496bf8710e90d7f812f
Galina Korzun <galinacernova473@gmail.com>
2024-11-06 00:06:15 +0300

    compareNumbers

---
+++ src/main/java/main/java/org/example/TaskNine.java
@@ -0,0 +1,19 @@
+package main.java.org.example;
+
+public class TaskNine {
+    public static void main(String[] args) {
+        int year = 2020;
+        System.out.println("Год"+year+"високосный"+LeapYear(year));
+    }
+    public static boolean LeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
