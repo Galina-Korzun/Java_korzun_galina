69620da78db785ffdf54e87c004dab3dd8279580
Galina Korzun <galinacernova473@gmail.com>
2024-11-06 00:03:29 +0300

    compareNumbers

---
+++ src/main/java/main/java/org/example/TaskFive.java
@@ -0,0 +1,13 @@
+package main.java.org.example;
+
+public class TaskFive {
+    public static void main(String[] args) {
+        System.out.println("expressionCheck(int a,int b,int c)");
+        int a=8;
+        int b=5;
+        int c=a+b;
+        if (c>=10 && c<=20) System.out.println(true);
+        else System.out.println(false);
+
+    }
+}
