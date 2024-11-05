095f9e7345c63fb90035da43abb56dd45374e7ee
Galina Korzun <galinacernova473@gmail.com>
2024-11-06 00:02:30 +0300

    compareNumbers

---
+++ src/main/java/main/java/org/example/TaskEleven.java
@@ -0,0 +1,13 @@
+package main.java.org.example;
+
+public class TaskEleven {
+    public static void main(String[] args) {
+        int[] arr=new int[100];
+        for (int a=0; a<arr.length; a++) {
+            arr[a]=a+1;
+        }
+        for (int element : arr) {
+            System.out.print(element + "");
+        }
+    }
+}
