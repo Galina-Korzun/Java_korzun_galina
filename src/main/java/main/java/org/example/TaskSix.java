a358a3f9b7032d4a7d9029d397a8dd994c2e633d
Galina Korzun <galinacernova473@gmail.com>
2024-11-06 00:07:18 +0300

    compareNumbers

---
+++ src/main/java/main/java/org/example/TaskSix.java
@@ -0,0 +1,13 @@
+package main.java.org.example;
+
+public class TaskSix {
+    public static void main(String[] args) {
+        int val=0;
+        if (val>=0) {
+            System.out.println("Number is positive");
+        }
+        else if (val<0) {
+            System.out.println("Number is negative");
+        }
+    }
+}
