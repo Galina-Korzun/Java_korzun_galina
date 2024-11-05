da09c2dfa46fa47dfe45021d0317d876009f5e6a
Galina Korzun <galinacernova473@gmail.com>
2024-11-06 00:09:09 +0300

    compareNumbers

---
+++ src/main/java/main/java/org/example/TaskTen.java
@@ -0,0 +1,18 @@
+package main.java.org.example;
+
+public class TaskTen {
+    public static void main(String[] args) {
+        int [] arr = {1,0,0,1,1,0,1,0,1,0,0,0,1,1};
+        for (int j=0; j<arr.length; j++) {
+            if (arr[j]==0) {
+                arr [j]=1;
+            } else if (arr[j]==1) {
+                arr[j]=0;
+            }
+        }
+        for (int element : arr) {
+            System.out.print(element+ "");
+        }
+    }
+}
+
