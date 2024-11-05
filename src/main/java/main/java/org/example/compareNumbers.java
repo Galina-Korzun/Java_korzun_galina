484508b3910a0435cfc7447f4f02e9d3ed2bcd7d
Galina Korzun <galinacernova473@gmail.com>
2024-11-05 23:54:03 +0300

    compareNumbers

---
+++ src/main/java/main/java/org/example/compareNumbers.java
@@ -0,0 +1,10 @@
+package main.java.org.example;
+
+public class compareNumbers {
+    public static void main(String[] args) {
+        System.out.println("compareNumbers()");
+        int a=1, b=2;
+        if (a>=b) System.out.println("a>=b");
+        else System.out.println("a<b");
+    }
+}
