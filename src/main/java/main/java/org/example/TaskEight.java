a32747d6c6febb673ae10e01a03244afdd91ad20
Galina Korzun <galinacernova473@gmail.com>
2024-11-06 00:01:05 +0300

    compareNumbers

---
+++ src/main/java/main/java/org/example/TaskEight.java
@@ -0,0 +1,14 @@
+package main.java.org.example;
+
+public class TaskEight {
+    public static void methodLineNumber() {
+        int number =7;
+        String line = "Hello, Aston!";
+        for (int i=0; i<number; i++) {
+            System.out.println(line);
+        }
+    }
+    public static void main(String[] args) {
+        methodLineNumber();
+    }
+}
