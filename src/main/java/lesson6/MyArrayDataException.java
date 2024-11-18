1a9dce260e4c39fa8b7f9852188c99ec1e2d495e
Galina Korzun <galinacernova473@gmail.com>
2024-11-18 09:32:30 +0300

    lesson6

---
+++ src/main/java/lesson6/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package lesson6;
+
+public class MyArrayDataException extends Exception {
+    public int a;
+    public int b;
+    MyArrayDataException(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
